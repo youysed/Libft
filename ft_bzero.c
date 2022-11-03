0dbf2de880801d96c7f241a10569f9f193f30ca0
youysed <youssefelhilaali66@gmail.com>
2022-11-03 17:38:09 +0100

    Second push

--- ft_bzero.c
+++ ft_bzero.c
@@ -25,12 +25,17 @@ RETURN VALUE
 */
 #include"libft.h"
 
+
+
+
+
 void	ft_bzero(void *s, size_t n)
 {
-	ft_memset(s, '\0', n);
+	ft_memset(s,0,n);
 }
 
-int main( void )
+/*     
+int main()
   {
     char b[10];
 
@@ -39,3 +44,7 @@ int main( void )
 	printf("%s",b);
     return (0);
   }
+
+
+*/
+
